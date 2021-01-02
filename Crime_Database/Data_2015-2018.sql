0dcec6c2008cc95e088e514f3845c28c6357dcc1
Tyler Vaughn <69045229+tylerlvaughn@users.noreply.github.com>
2021-01-02 10:24:19 -0800

    Database Proof

---
+++ Crime_Database/Data_2015-2018.sql
@@ -0,0 +1,78 @@
+DROP TABLE "crime_data_2015";
+
+--Create New Table
+CREATE TABLE "crime_data_2015" (
+	"council_district" INT,
+	"highest_offense_desc" VARCHAR,
+	"highest_offense" VARCHAR,
+	"date_rec" DATE,
+	"location_rec" VARCHAR,
+	"district" VARCHAR,
+	"zip" INT,
+	"x_coord" INT,
+	"y_coord" INT
+	
+	
+);
+
+-- View Table Columns and Datatypes
+SELECT * FROM crime_data_2015;
+
+DROP TABLE crime_data_2016;
+
+--Create New Table
+CREATE TABLE crime_data_2016 (
+	council_district INT,
+	highest_offense_desc VARCHAR,
+	highest_offense VARCHAR,
+	date_rec DATE,
+	location_rec VARCHAR,
+	district VARCHAR,
+	zip INT,
+	x_coord INT,
+	y_coord INT
+	
+	
+);
+
+-- View Table Columns and Datatypes
+SELECT * FROM crime_data_2017;
+
+--Create New Table
+CREATE TABLE crime_data_2017 (
+	council_district INT,
+	highest_offense_desc VARCHAR,
+	highest_offense VARCHAR,
+	date_rec DATE,
+	location_rec VARCHAR,
+	district VARCHAR,
+	zip INT,
+	x_coord INT,
+	y_coord INT
+	
+	
+);
+
+-- View Table Columns and Datatypes
+SELECT * FROM crime_data_2017;
+
+-- View Table Columns and Datatypes
+SELECT * FROM crime_data_2018;
+
+--Create New Table
+CREATE TABLE crime_data_2018 (
+	council_district INT,
+	highest_offense_desc VARCHAR,
+	highest_offense VARCHAR,
+	date_rec DATE,
+	location_rec VARCHAR,
+	district VARCHAR,
+	zip INT,
+	x_coord INT,
+	y_coord INT
+	
+	
+);
+
+-- View Table Columns and Datatypes
+SELECT * FROM crime_data_2018;
